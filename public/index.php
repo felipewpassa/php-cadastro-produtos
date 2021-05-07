73198b4c446d3e6716142dcff71e80be59055ef5
Felipe Passa <felipe_passa@hotmail.com>
2021-05-07 20:37:08 -0300

    Adicionado bootstrap

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@ include './../app/Libraries/Controller.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=APP_NAME?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" >
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
@@ -18,6 +19,8 @@ include './../app/Libraries/Controller.php';
         $routes = new Route();
     ?>
 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
     <script src="<?=URL?>/public/js/index.js"></script>
 </body>
 </html>
