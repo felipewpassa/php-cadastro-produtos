92b17e9cc7db6da36557cd49650e8cdd579cf387
Felipe Passa <felipe_passa@hotmail.com>
2021-05-08 01:53:03 -0300

    ajuste na constante URL

--- app/config.php
+++ app/config.php
@@ -2,4 +2,4 @@
 
 define('APP', dirname(__FILE__));
 define('APP_NAME', 'AstrusWeb');
-define('URL', 'http://localhost/astrusweb-cadProdutos/');
+define('URL', 'http://localhost/astrusweb-cadProdutos');
