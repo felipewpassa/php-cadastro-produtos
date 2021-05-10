d243e246fb6fac58a4739eef6979a9ed13c2ae8e
Felipe Passa <felipe_passa@hotmail.com>
2021-05-10 00:29:11 -0300

    Melhoria da listagem de categoria

--- app/Views/pages/categorias/index.php
+++ app/Views/pages/categorias/index.php
@@ -5,11 +5,22 @@
             <a href="<?=URL?>/categorias/cadastrar" class="btn btn-primary">Cadastrar</a>
         </div>
         <div class="card-body">
-            <ul class="list-group list-group-flush">
-                <?php foreach ($data['categorias'] as $categoria): ?>
-                    <li class="list-group-item"><?= $categoria->dsCategoria ?></li>
-                <?php endforeach ?> 
-            </ul>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Descrição</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($data['categorias'] as $key => $categoria): ?>
+                        <tr class="<?= $key%2!=0 ? 'table-light' : '' ?>">
+                            <td><?= $categoria->idCategoria ?></td>
+                            <td><?= substr($categoria->dsCategoria, 0, 30) ?></td>
+                        </tr> 
+                    <?php endforeach ?> 
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
