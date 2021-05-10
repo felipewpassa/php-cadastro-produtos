b91cf518ed900729ef00cebe91c915eb7f9d8590
Felipe Passa <felipe_passa@hotmail.com>
2021-05-10 19:03:04 -0300

    Controlador e page index de produtos

---
+++ app/Controllers/Produtos.php
@@ -0,0 +1,7 @@
+<?php
+
+class Produtos extends Controller {
+    public function index() {
+        $this->view('pages/produtos/index');
+    }
+}
---
+++ app/Views/pages/produtos/index.php
@@ -0,0 +1,10 @@
+<div class="container py-4">
+    <div class="card">
+        <div class="card-header">
+            Produtos
+        </div>
+        <div class="card-body">
+
+        </div>
+    </div>
+</div>
