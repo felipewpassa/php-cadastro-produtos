90db2e98593b7a349909cfb2b19ad143b2200199
Felipe Passa <felipe_passa@hotmail.com>
2021-05-13 19:25:37 -0300

    Ajuste design formulario de produtos

--- app/Views/pages/produtos/cadastrar.php
+++ app/Views/pages/produtos/cadastrar.php
@@ -6,41 +6,50 @@
         <?=Session::alert('Produto')?>
         <div class="card-body">
             <form name="categoria" method="POST" action="<?=URL?>/produtos/cadastrar" enctype="multipart/form-data">
-                <input id="nmProduto" name="nmProduto" type="text" placeholder="nmProduto"
-                class="form-control <?= $data['nmProdutoErro'] ? 'is-invalid' : '' ?>">
-                <div class="invalid-feedback">
-                    <?= $data['nmProdutoErro'] ?>
+                <div class="mb-2">
+                    <label for="nmProduto" class="form-label">*Nome:</label>
+                    <input id="nmProduto" name="nmProduto" type="text" class="form-control <?= $data['nmProdutoErro'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $data['nmProdutoErro'] ?>
+                    </div>
                 </div>
 
-                <input id="dsProduto" name="dsProduto" type="text" placeholder="dsProduto"
-                class="form-control <?= $data['dsProdutoErro'] ? 'is-invalid' : '' ?>">
-                <div class="invalid-feedback">
-                    <?= $data['dsProdutoErro'] ?>
+                <div class="mb-2">
+                    <label for="dsProduto" class="form-label">*Descrição:</label>
+                    <input id="dsProduto" name="dsProduto" type="text" class="form-control <?= $data['dsProdutoErro'] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $data['dsProdutoErro'] ?>
+                    </div>
                 </div>
 
-                <select id="idCategoria" name="idCategoria" class="form-select <?= $data['idCategoriaErro'] ? 'is-invalid' : '' ?>">
-                    <option value="0" selected>Selecione uma categoria</option>
-                    <?php foreach($data['categorias'] as $categoria): ?>
-                        <option value="<?= $categoria->idCategoria ?>">
-                            <?= $categoria->dsCategoria ?>
-                        </option>
-                    <?php endforeach ?>
-                </select>
-                <div class="invalid-feedback">
-                    <?= $data['idCategoriaErro'] ?>
+                <div class="mb-2">
+                    <label for="idCategoria" class="form-label">*Categoria:</label>
+                    <select id="idCategoria" name="idCategoria" class="form-select <?= $data['idCategoriaErro'] ? 'is-invalid' : '' ?>">
+                        <option value="0" selected>Selecione uma categoria</option>
+                        <?php foreach($data['categorias'] as $categoria): ?>
+                            <option value="<?= $categoria->idCategoria ?>">
+                                <?= $categoria->dsCategoria ?>
+                            </option>
+                        <?php endforeach ?>
+                    </select>
+                    <div class="invalid-feedback">
+                        <?= $data['idCategoriaErro'] ?>
+                    </div>
                 </div>
-
-                <div class="input-field mt-5">
-                    <table id="table_field">
+                <div class="mt-4">
+                    <table class="table table-bordered" id="table_field">
                         <tr>
-                            <td>Descricao</td>
-                            <td>Imagem</td>
-                            <td><input class="btn btn-primary" type="button" name="add" id="add" value="Adicionar"></td>
+                            <th>Descrição</th>
+                            <th>Imagem</th>
+                            <th>
+                                <input class="btn btn-sm btn-secondary" type="button" name="add" id="add" value="Adicionar">
+                            </td>
                         </tr>
                     </table>
                 </div>
-
-                <input type="submit" value="Salvar" class="btn btn-primary mt-5">
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                    <input type="submit" value="Salvar" class="btn btn-primary">
+                </div>
             </form>
 
         </div>
@@ -51,9 +60,24 @@
     $(document).ready(function() {
         var html = `
         <tr>
-            <td><input class="form-control" type="text" name="dsImagem[]"></td>
-            <td><input class="form-control" type="file" name="image[]" accept="image/jpeg, image/png"></td>
-            <td><input class="btn btn-danger" type="button" name="remove" id="remove" value="Remover"></td>
+            <td>
+                <div class="input-group input-group-sm">
+                    <input class="form-control" type="text" name="dsImagem[]">
+                </div>
+            </td>
+            <td>
+                <div class="input-group input-group-sm">
+                    <input class="form-control" type="file" name="image[]" accept="image/jpeg, image/png">
+                </div>
+            </td>
+            <td>
+                <div class="input-group input-group-sm">
+                    <button class="btn btn-light" type="button" name="remove" id="remove">
+                        <i class="bi bi-x-lg"></i>
+                        Remover
+                    </button>
+                </div>
+            </td>
         </tr>`;
         $("#add").click(function() {
             $("#table_field").append(html);
